3d53c3169ca186f55dd123f431cc11bb8d5f7053
Beliskner <beliskner.a@gmail.com>
2019-03-10 16:02:49 +0300

    delete space in 6 string

--- network.py
+++ network.py
@@ -2,7 +2,6 @@
 import random
 
 r = [random.randint(0, 255) for i in range(0, 4)]	#rand part of IP address
-mask = random.randint(7, 30) 				#partof network MASK
-							#just skip this
+mask = random.randint(7, 30) 				#part of network MASK
 print(*r, sep='.', end='/', )				#display address
 print(mask)						#and mask, and that all
